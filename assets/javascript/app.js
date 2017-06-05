c3f56d8f4fe17c752cc7899f6a4089249cc9eb6c
kirk <kirk733@MacBook.local>
2017-06-04 21:29:43 -0600

    I have radio button true/false check and win/loss working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,10 @@ $(document).ready(function() {
 
      // Here we are provided an initial array of letters.
 
-     var numQues = 1;
+     var numQues = 0;
+     var wins = 0;
+     var losses = 0;
+
      var question = ["Which of these actors played in the movie Forrest Gump?", 
       	"What movie did this famous qoute come from 'My biological clock is ticking'?"];
 
@@ -13,16 +16,29 @@ $(document).ready(function() {
      var answer2 = ["Tom Cruise", "Your mama"];
      var answer3 = ["Tom Petty", "My Cousin Vinny"];
      var answer4 = ["Tom Hanks", "My Cousin Desiree"];
-
+     var trueAnswer = ["Tom Hanks", "My Cousin Vinny"];
 
       	//$("#myList").hide();
 		$("input").hide();	
 		$("label").hide();
+		$("#button2").hide();
 
       	$("#button1").on("click", function() {
       		$("input").show();	
 			$("label").show();
+			$("#button2").show();
+			$("#button1").hide();
+
       		answerQuestion(); 
+
+      	});
+
+      	$("#button2").on("click", function() {
+      		
+      		checkAnswer();
+      		answerQuestion();
+
+      		//answerQuestion(); 
 
       	});
 
@@ -30,15 +46,48 @@ $(document).ready(function() {
       	function answerQuestion(){
       		// Inside the on-click event...
       		if(numQues <= 6){
-				$("#question1").html(question[0]);
-      			$('label[for=answer1]').html(answer1[0]);
-      			$('label[for=answer2]').html(answer2[0]);
-      			$('label[for=answer3]').html(answer3[0]);
-      			$('label[for=answer4]').html(answer4[0]);
-      		console.log(numQues);
-      		console.log(question1.answer1)
+				$("#question1").html(question[numQues]);
+      			$('label[for=answer1]').html(answer1[numQues]);
+      			$('label[for=answer2]').html(answer2[numQues]);
+      			$('label[for=answer3]').html(answer3[numQues]);
+      			$('label[for=answer4]').html(answer4[numQues]);
+      			numQues++
       		};
       		};
+
+      	function checkAnswer(){	
+    		var w = document.getElementById("test1").checked;
+    		var x = document.getElementById("test2").checked;
+    		var y = document.getElementById("test3").checked;
+    		var z = document.getElementById("test4").checked;
+    		if(w == true){
+    			retAnswer = answer1[numQues-1];
+    		}
+    		if(x == true){
+    			retAnswer = answer2[numQues-1];
+    		}
+    		if(y == true){
+    			retAnswer = answer3[numQues-1];
+    		}
+    		if(z == true){
+    			retAnswer = answer4[numQues-1];
+    		}
+
+    		if (trueAnswer[numQues-1] === retAnswer){
+    			wins++
+    		}
+
+    		else {
+    			losses++
+    		}
+
+
+    		console.log(trueAnswer[numQues-1]);
+    		console.log(w, x, y, z);
+    		console.log(retAnswer);
+    		console.log("Wins" + wins);
+    		console.log("Losses" + losses);
+			};
 
     });
 
